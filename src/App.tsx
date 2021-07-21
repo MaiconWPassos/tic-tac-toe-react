93a7c236239ead6e94232dd6a3a7ce0ee1b50913
Maicon W. Passos <maicon@mwps.com.br>
2021-07-21 15:57:25 -0300

    update LINK DONATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-
-
-import Board from './components/Board';
-import './styles/app.css';
+import Board from "./components/Board";
+import "./styles/app.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Tic Tac Toe</h1>
+        <h1>Tic Tac Toe</h1>
       </header>
-      <Board/>
+      <Board />
+    
+
+      <div className="donate-link">
+        <a href="https://www.paypal.com/donate?hosted_button_id=XC4TGLL84VX3L">Buy me a coffee ☕ </a>
+      </div>
     </div>
   );
 }
